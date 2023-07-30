de4b00094c2178ca064118e8b89534fe9edec828
Juan Andres Lopez Motta <juanandres.lopez@crehana.com>
2023-07-29 21:44:22 -0500

    feat: added authenticated healthcheck mutation

--- app/mutations/healthcheck.py
+++ app/mutations/healthcheck.py
@@ -1,11 +1,25 @@
 import strawberry
 
+from app.config.permissions import IsAuthenticated
+from app.dependencies.graphql_context import CustomInfo
 from app.schemas.healthcheck import HealthCheckQuryOutput
+
 
 @strawberry.type
 class HealthcheckMutation:
-    @strawberry.mutation(description="Returns the message given in the input")
-    def healthcheck(self, message: str) -> HealthCheckQuryOutput:
-        return HealthCheckQuryOutput(
-            message=message
-        )
+    @strawberry.mutation(
+        description="Returns the message given in the input",
+    )
+    def healthcheck(
+        self, message: str, info: CustomInfo
+    ) -> HealthCheckQuryOutput:
+        return HealthCheckQuryOutput(message=message)
+
+    @strawberry.mutation(
+        description="Returns the message given in the input only if user is authenticated",
+        permission_classes=[IsAuthenticated],
+    )
+    def authenticated_healthcheck(
+        self, message: str, info: CustomInfo
+    ) -> HealthCheckQuryOutput:
+        return HealthCheckQuryOutput(message=message)
