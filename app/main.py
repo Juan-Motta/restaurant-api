9ad2b41cb6449b40a799a01f1477ed938a8f62f5
Juan Andres Lopez Motta <juanandres.lopez@crehana.com>
2023-07-29 19:15:36 -0500

    fix: removed code in lifespan method

--- app/main.py
+++ app/main.py
@@ -9,31 +9,6 @@ from app.routes import router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # start up
-    from sqlalchemy import select
-
-    from app.database.base import Session
-    from app.models import User, UserProfile
-
-    session = Session()
-    # user = User(
-    #     first_name="Jhon",
-    #     last_name="Doe",
-    #     email="jhon@example.com",
-    #     nid="12345678",
-    # )
-    # user.set_password(password="12345678")
-    # session.add(user)
-    # session.commit()
-    # query = select(User).select_from(User).where(User.id == 1)
-    # user = session.execute(query).scalars().first()
-    # print(user)
-    # user_profile = UserProfile(
-    #     user_id=user.id,
-    #     profile=ProfileEnum.APP_ADMINISTRATOR
-    # )
-    # session.add(user_profile)
-    # session.commit()
-    # print(user.profiles)
     yield
     # shut down
 
