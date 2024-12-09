293a8123ddd03a556ed91282ff146fc20498cb6d
Juan Motta <juan.motta@simetrik.com>
2024-12-08 22:38:51 -0500

    chore: remove unused dependencies from generate db data script

--- scripts/generate_db_data.py
+++ scripts/generate_db_data.py
@@ -1,13 +1,9 @@
-from faker import Faker
-
 from tests.factories.menu_item_factory import MenuItemFactory
 from tests.factories.order_factory import OrderFactory
 from tests.factories.order_item_factory import OrderItemFactory
 from tests.factories.rating_factory import RatingFactory
 from tests.factories.restaurant_factory import RestaurantFactory
 from tests.factories.user_factory import UserFactory
-
-faker = Faker()
 
 
 def generate_data():
